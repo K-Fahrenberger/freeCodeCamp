7a9499c4a65e3ae69c3edf5f8a8274181986281a
Kevin Fahrenberger <fahrenberger.k@web.de>
2024-01-25 10:41:02 +0100

    feat: 3.02 challenge

--- 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
@@ -1,4 +1,16 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
+let firstCard = getRandomInt(2, 12)
+let secondCard = getRandomInt(2, 12)
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+  let sum = firstCard + secondCard
 // 2. Create a variable, sum, and set it to the sum of the two cards
+console.log(firstCard)
+console.log(secondCard)
+console.log(sum)
