7a987b444373a1f43b43d394336978be8bc235a4
Kevin Fahrenberger <fahrenberger.k@web.de>
2024-01-27 13:56:25 +0100

    feat: 4.06

--- 4. Practice time - part 2/06. Rock papers scissors/index.js
+++ 4. Practice time - part 2/06. Rock papers scissors/index.js
@@ -2,4 +2,15 @@ let hands = ["rock", "paper", "scissor"]
 
 // Create a function that returns a random item from the array
 
-
+function randomItem() {
+    let randomNumber = Math.random()
+    if (randomNumber <0.33){
+        return hands[0]
+    } else if (randomNumber <0.66){
+        return hands[1]
+    } else{
+        return hands[2]
+    }
+}
+let randomHand = randomItem()
+console.log(randomHand)
